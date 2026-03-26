93d9b337941056f12f688de719bd1336ba662694
songeseudeo <qetuo4751@naver.com>
2026-03-26 21:14:49 +0900

    Add files via upload

---
+++ 03-Vue/260326/심화 문제/v-model-test/src/components/InputName.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <input type="text" 
+        :value="name"
+        @input="$emit('update:name', $event.target.value)"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "InputName",
+        props: ['name'],
+    }
+</script>
