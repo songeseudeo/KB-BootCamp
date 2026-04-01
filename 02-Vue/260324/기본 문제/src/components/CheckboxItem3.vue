fcad151974f8a579a5de29bf964e7b1954eb32e9
songeseudeo <qetuo4751@naver.com>
2026-04-01 14:37:44 +0900

    Add files via upload

---
+++ 02-Vue/260324/기본 문제/src/components/CheckboxItem3.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <input type="checkbox" :checked="checked"> {{ name }} (ID: {{ id }})
+  </li>
+</template>
+
+<script>
+export default {
+  name: "CheckboxItem3",
+  props: {
+    // 1. id는 숫자 또는 문자열만 허용
+    id: [Number, String],
+    
+    // 2. name은 반드시 문자열이어야 함
+    name: {
+      type: String,
+      required: true
+    },
+    
+    // 3. checked는 불리언(T/F), 안 오면 기본값은 false
+    checked: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
---
+++ 02-Vue/260324/기본 문제/src/main.js
@@ -0,0 +1,16 @@
+// //전역 컴포넌트
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import CheckboxItem from './components/CheckboxItem.vue'
+
+// createApp(App)
+// .component('CheckboxItem', CheckboxItem)
+// .mount('#app');
+
+
+//지역 컴포넌트
+import { createApp } from 'vue'
+import App from './App2.vue'
+
+createApp(App)
+.mount('#app');
